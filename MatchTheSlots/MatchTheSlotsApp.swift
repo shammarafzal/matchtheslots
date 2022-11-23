837a15a1ea7a8028f7c06b2adf9a68391ea864a6
Ammar Afzal <ammarafzal075@gmail.com>
2022-11-23 17:44:05 +0500

    Initial Commit

---
+++ MatchTheSlots/MatchTheSlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchTheSlotsApp.swift
+//  MatchTheSlots
+//
+//  Created by Ammar Afzal on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MatchTheSlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
